dd0260bc6d02a20a562c381c356bb1caa1d13d76
Ahmed Abdelgawad <hagahm721@gmail.com>
2025-10-16 15:14:32 +0300

    finish anime_categories view UI

--- lib/features/home/view/anime_view.dart
+++ lib/features/home/view/anime_view.dart
@@ -1,10 +1,50 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
+import 'package:movix/features/home/data/categoriy_movie_card_model.dart';
+import 'package:movix/features/home/widgets/category_movie_card.dart';
 
 class AnimeView extends StatelessWidget {
-  const AnimeView({super.key});
+  AnimeView({super.key});     
+  List<CategoriyMovieCardModel> animeList = [ // List of anime categories with images and titles in the same model (CategoriyMovieCardModel)
+    CategoriyMovieCardModel(movieImage: 'assets/images/anime_categories/anime action.jpeg',title: 'Action',),
+    CategoriyMovieCardModel(movieImage: 'assets/images/anime_categories/anime adventure.jpeg',title: 'Adventure'),
+    CategoriyMovieCardModel(movieImage: 'assets/images/anime_categories/anime comdy.jpeg',title: 'Comedy'),
+    CategoriyMovieCardModel(movieImage: 'assets/images/anime_categories/anime drama.jpeg',title: 'Drama',),
+    CategoriyMovieCardModel(movieImage: 'assets/images/anime_categories/anime fantasy.jpeg',title: 'Fantasy',),
+    CategoriyMovieCardModel(movieImage: 'assets/images/anime_categories/anime horror.jpeg',title: 'Horror',),
+    CategoriyMovieCardModel(movieImage: 'assets/images/anime_categories/anime hostrical.jpeg',title: 'Historical',),
+    CategoriyMovieCardModel(movieImage: 'assets/images/anime_categories/anime mystry.jpeg',title: 'Mystery'),
+    CategoriyMovieCardModel(movieImage: 'assets/images/anime_categories/anime romance.jpeg',title: 'Romance'),
+    CategoriyMovieCardModel(movieImage: 'assets/images/anime_categories/anime sci fi.jpeg',title: 'Sci-Fi'),
+    CategoriyMovieCardModel(movieImage: 'assets/images/anime_categories/anime sports.jpeg',title: 'Sports'),
+    CategoriyMovieCardModel(movieImage: 'assets/images/anime_categories/anime thriller.jpeg', title: 'Thriller'),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        // backgroundColor: Colors.white,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: GridView.builder(
+            itemCount: animeList.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.6,
+            ),
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: EdgeInsets.all(10),
+                child: CategoryMovieCard(
+                  categoriyMovieCardModel: animeList[index],
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
